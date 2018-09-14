af0c4cc3c491b980fb4b2d100d3ccea4b3207b71
Callum <43255759+C067@users.noreply.github.com>
2018-09-13 21:11:25 -0400

    Add files via upload

---
+++ WS01/in_lab/graph.h
@@ -0,0 +1,25 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+/*
+Name: Callum Dodge
+Workshop: Workshop 1
+Date: May 5, 2018
+Section: OOP244SDD
+
+*/
+
+// Fills the samples array with the statistic samples
+void getSamples(int samples[], int noOfSamples);
+
+// Finds the largest sample in the samples array, if it is larger than 70,
+// therwise it will return 70. 
+int findMax(int samples[], int noOfSamples);
+
+// Prints a scaled bar relevant to the maximum value in samples array
+void printBar(int val, int max);
+
+// Prints a graph comparing the sample values visually 
+void printGraph(int samples[], int noOfSamples);
+
+#endif // !GRAPH_H
---
+++ WS01/in_lab/tools.h
@@ -0,0 +1,18 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+/*
+Name: Callum Dodge
+Workshop: Workshop 1
+Date: May 5, 2018
+Section: OOP244SDD
+
+*/
+
+// Performs a fool-proof integer entry
+int getInt(int min, int max);
+
+// Displays the user interface menu
+int menu();
+
+#endif // !TOOLS_H
